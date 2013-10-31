89d5a1a0d34a1a0bf0b17bb75f58264b0626c0db
Francisco A. Lozano <flozano@gmail.com>
2013-10-31 18:22:50 +0100

    Reformat of DeliveryResult lib

--- src/main/java/com/notnoop/apns/DeliveryResult.java
+++ src/main/java/com/notnoop/apns/DeliveryResult.java
@@ -1,57 +1,57 @@
 package com.notnoop.apns;
 
 public class DeliveryResult {
-	private DeliveryError error;
-	private int id;
+    private DeliveryError error;
+    private int id;
 
-	public DeliveryResult(DeliveryError error, int id) {
-		this.error = error;
-		this.id = id;
-	}
+    public DeliveryResult(DeliveryError error, int id) {
+        this.error = error;
+        this.id = id;
+    }
 
-	public DeliveryError getError() {
-		return error;
-	}
+    public DeliveryError getError() {
+        return error;
+    }
 
-	public void setError(DeliveryError error) {
-		this.error = error;
-	}
+    public void setError(DeliveryError error) {
+        this.error = error;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((error == null) ? 0 : error.hashCode());
-		result = prime * result + id;
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((error == null) ? 0 : error.hashCode());
+        result = prime * result + id;
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		DeliveryResult other = (DeliveryResult) obj;
-		if (error != other.error)
-			return false;
-		if (id != other.id)
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DeliveryResult other = (DeliveryResult) obj;
+        if (error != other.error)
+            return false;
+        if (id != other.id)
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return String.format("DeliveryResult [error=%s, id=%s]", error, id);
-	}
+    @Override
+    public String toString() {
+        return String.format("DeliveryResult [error=%s, id=%s]", error, id);
+    }
 }
